544461d6643fa82bdcc3b0bf33ad2508e8d3a1a9
heesane <tees3359@gmail.com>
2024-06-21 23:33:08 +0900

    Diary

---
+++ weather/src/main/java/zero/weather/domain/Diary.java
@@ -0,0 +1,32 @@
+package zero.weather.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Diary {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String weather;
+
+    private String icon;
+
+    private Double temperature;
+
+    private String text;
+
+    private LocalDate date;
+}
