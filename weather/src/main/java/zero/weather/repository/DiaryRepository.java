760398358873677e34bfbc160fabf4e81409c943
heesane <tees3359@gmail.com>
2024-06-24 19:37:17 +0900

    feat: Diary Repository

--- weather/src/main/java/zero/weather/repository/DiaryRepository.java
+++ weather/src/main/java/zero/weather/repository/DiaryRepository.java
@@ -1,9 +1,21 @@
 package zero.weather.repository;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import zero.weather.domain.Diary;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
+    List<Diary> findAllByDate(LocalDate date);
+
+    List<Diary> findAllByDateBetween(LocalDate fromDate, LocalDate toDate);
+
+    Diary getFirstByDate(LocalDate date);
+
+    @Transactional
+    void deleteByDate(LocalDate date);
 }
